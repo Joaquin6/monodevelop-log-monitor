111ce4b6814a3ee8bed745bcb08861b6ee5901ff
joaquin6 <joaquinbriceno1@gmail.com>
2019-11-26 12:52:44 -0600

    Updated packages

--- src/MonoDevelop.LogMonitor/MonoDevelop.LogMonitor/CurrentIdeLogFile.cs
+++ src/MonoDevelop.LogMonitor/MonoDevelop.LogMonitor/CurrentIdeLogFile.cs
@@ -1,30 +1,4 @@
-﻿//
-// CurrentIdeLogFile.cs
-//
-// Author:
-//       Matt Ward <matt.ward@microsoft.com>
-//
-// Copyright (c) 2018 Microsoft
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-using System;
+﻿using System;
 using System.IO;
 using MonoDevelop.Core;
 using MonoDevelop.Ide;
@@ -34,14 +8,14 @@ namespace MonoDevelop.LogMonitor
 {
 	static class CurrentIdeLogFile
 	{
-		static CurrentIdeLogFile ()
+		static CurrentIdeLogFile()
 		{
 			FileName = UserProfile.Current.LogDir.Combine ("Ide.log");
 		}
 
 		public static FilePath FileName { get; private set; }
 
-		public static void Update ()
+		public static void Update()
 		{
 			try {
 				if (!File.Exists (FileName))
@@ -53,10 +27,10 @@ namespace MonoDevelop.LogMonitor
 			}
 		}
 
-		public static void Open ()
+		public static void Open()
 		{
-			var fileInfo = new FileOpenInformation (FileName);
-			IdeApp.Workbench.OpenDocument (fileInfo).Ignore ();
+			var fileInfo = new FileOpenInformation(FileName);
+			IdeApp.Workbench.OpenDocument(fileInfo).Ignore();
 		}
 	}
 }
